21ea42588b7255ee3a8228bd4e410c552423c03c
DevMon <devmon@Monsurus-MacBook-Air.local>
2024-09-28 21:09:12 +0100

    initial commit
    add basic project setup

---
+++ src/main/java/com/mon/Main.java
@@ -0,0 +1,12 @@
+package com.mon;
+
+/**
+ * create an ioc container that:
+ * create objects (singleton and prototype scoped)
+ * inject objects (singleton and prototype scoped)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
