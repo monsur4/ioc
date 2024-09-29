0110f12de241a6983add30f3f84954f9a4aadac7
Monsuru Okuniyi <okuniyimonsuru@yahoo.com>
2024-09-29 12:28:58 +0100

    update IOCContainer interface and implementation
    - add method to get object and create objects

--- src/main/java/com/mon/ioc/container/IOCContainer.java
+++ src/main/java/com/mon/ioc/container/IOCContainer.java
@@ -1,4 +1,6 @@
 package com.mon.ioc.container;
 
 public interface IOCContainer {
+    Object getObject(Class<?> clazz);
+    void createObjects();
 }
--- src/main/java/com/mon/ioc/container/impl/IOCContainerImpl.java
+++ src/main/java/com/mon/ioc/container/impl/IOCContainerImpl.java
@@ -1,10 +1,45 @@
 package com.mon.ioc.container.impl;
 
 import com.mon.ioc.container.IOCContainer;
+import com.mon.ioc.scan.ClassDefinition;
+import com.mon.ioc.scan.ClassScanner;
+import com.mon.ioc.service.ObjectCreator;
 
 /**
  * This is the implementation of the container that stores instances of objects
  * and also injects them into other classes
  */
 public class IOCContainerImpl implements IOCContainer {
+
+    private ClassScanner classScanner;
+
+    private ObjectCreator objectCreator;
+
+    private Class<?> startUpClass;
+
+    public IOCContainerImpl() {
+    }
+
+    public IOCContainerImpl(
+            ClassScanner classScanner,
+            ObjectCreator objectCreator,
+            Class<?> startUpClass
+    ) {
+        this.classScanner = classScanner;
+        this.objectCreator = objectCreator;
+        this.startUpClass = startUpClass;
+        createObjects();
+    }
+
+    public void createObjects(){
+        classScanner.scan(startUpClass);
+        for(ClassDefinition classDefinition: classScanner.getClassDefinitions()){
+            objectCreator.create(classDefinition);
+        }
+    }
+
+    public Object getObject(Class<?> clazz){
+        ClassDefinition classDefinition = classScanner.getClassDefinition(clazz);
+        return objectCreator.create(classDefinition);
+    }
 }
